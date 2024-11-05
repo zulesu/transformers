ee8f6dc83b312228515f63e3a2214eeb457caa8c
Matt <rocketknight1@gmail.com>
2024-11-05 18:13:56 +0000

    Remove breakpoint

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2298,7 +2298,6 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
                 "Special tokens have been added in the vocabulary, make sure the associated word embeddings are"
                 " fine-tuned or trained."
             )
-        breakpoint()
         return tokenizer
 
     @staticmethod
